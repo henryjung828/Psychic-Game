8da541ca696fae72bdd72926d307c15d77a7ea49
hjung repository <henryjung828@gmail.com>
2019-09-09 19:57:58 -0500

    Games adjusted
    revision to guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,57 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 var userWins = 0;
 var userLoses = 0;
-var guessLeft = 0;
-var userGuess;
+var guessLeft = 9;
+var userGuessed = [];
+var eachofLetter = null;
 var psychicGuess;
 var letterToGuess;
-var guessedletter; = [];
+var winsP = document.getElementById("wins");
+var losesP = document.getElementById("loses");
 
 
 document.onkeyup = function(event) {
+        var userGuess = event.key; 
     var userGuess = event.key; 
-        guessedLetters.push(userGuess);
+        var userGuess = event.key; 
+            guessedLetters.push(userGuess);
+    var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
+
+    function countGuessleft () {
+        document.querySelector("#guessLeft").innerHTML = "Guess Left:" + guessLeft;
+    }
+
+    function soFarUserGuessed () {
+        document.querySelector("#letters").innerHTML = "Guess so Far:" + userGuessed.join('');
+
+    }
+    
+    function countGuessleft();
+
+    var restart = function() {
+        guessLeft = 9;
+        userGuessed = [];
+        var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
+    }
+    document.onkeyup = function(event) {
+        guessLeft--;
+        
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        
+        guessedLetter.push(userGuess);
+        countGuessleft();
+        soFarUserGuessed();
+
     var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
         if (userGuess === psychicGuess) {
              userWin++;
-        guessesLeft = 25;
-       } else {
-           guessLeft--;
-       }
-       if (guessLeft === 0) {
-           userLoses++;
-           guessesLeft = 25; 
-           // just added a random number ask in class
-       }
-    }
-
-  var winsP = document.getElementById("wins");
-  var losesP = document.getElementById("loses");
-  var guessLeft = document.getElementById("guessLeft");
-  var letterGuessed = document.getElementById("guessed");
-
-
-// function adder(x,y,z) {
-
-//     function consoleInside(letters) {
-//          for (var i = 0; i < letters.length; i++) {
-//             console.log(letters[i]);
-//         };
-
-     directionsText.textContent = "";
-     userChoiceText.textContent = "You chose: " + userGuess;
-     computerChoiceText.textContent = "The computerchose: " + psychicGuess;
-     winsText.textContent = "wins: " + userWins;
-     lossesText.textContent = "losses: " + userLoses;
-
-//     };
-// };
+             document.querySelector("#wins").innerHTML = "Wins: " + userWins;
+             restart();
+       } else if (guessLeft === 0) {
+             userLoses++;
+             document.querySelector("#loses").innerHTML = "Loses: " + userLoses;
+             restart();
+            };
+             };    
+            };  
+  
